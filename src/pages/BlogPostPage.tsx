67043201756029d43abac95f8f70246709a46c7f
djatkovstincina <ndjatkov@gmail.com>
2025-03-08 09:29:47 +0100

    TASK 5 - When on the blog post page, you should be able to click a Back button to get back to the users list page

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import { selectBlogPosts } from "../redux/blog/blogSlice";
 
 const BlogPostPage = () => {
     const { postId } = useParams();
+    const navigate = useNavigate();
     const blogPosts = useTypedSelector(selectBlogPosts);
+    
     const post = blogPosts.find((p) => p.id === postId);
 
     if (!post) {
@@ -15,6 +17,12 @@ const BlogPostPage = () => {
 
     return (
         <div style={{ marginTop: "40px" }}>
+            <button 
+                onClick={() => navigate(-1)} 
+                style={{marginBottom: "32px", padding: "12px", cursor: "pointer"}}
+            >
+                Back to Users list
+            </button>
             <h1 style={{ marginBottom: "24px" }}>{post.title}</h1>
             <p style={{ marginBottom: "18px" }}><strong>Author ID:</strong> {post.userId}</p>
             <p>{post.body}</p>
